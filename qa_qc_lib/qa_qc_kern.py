2e18dca8a03fbacddf531b4e601db2392abdd3ac
Salnikov-MaX <68896657+Salnikov-MaX@users.noreply.github.com>
2023-07-03 15:23:04 +0700

    QA_QC

---
+++ qa_qc_lib/qa_qc_kern.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+class QA_QC_kern():
+    def __init__(self, file_path:str) -> None:
+        """_summary_
+
+        Args:
+            data (str): _description_
+        """ 
+        pass
+
+
+    def first_test(self) -> bool:
+        """_summary_
+
+        Returns:
+            bool: _description_
+        """        
+        return True
+
+
+    def second_test(self) -> bool:
+        """_summary_
+
+        Returns:
+            bool: _description_
+        """        
+        return True
+
+
+    def get_list_of_tests(self) -> list:
+        """_summary_
+
+        Returns:
+            list: _description_
+        """        
+        return ['first_test', 'second_test']
+
+
+    def start_tests(self, list_of_tests:list) -> None:
+        """_summary_
+
+        Args:
+            list_of_tests (list): _description_
+        """        
+        pass
+
+
+    def generate_test_report(self) -> str:
+        """_summary_
+
+        Returns:
+            str: _description_
+        """        
+        return 'test results'
