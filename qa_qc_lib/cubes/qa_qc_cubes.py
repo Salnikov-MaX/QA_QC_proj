c4428cdeb9c08b298a8362176978dbf7497facda
Rucard <www.carat@list.ru>
2023-09-12 02:41:37 +0700

    add first-order tests

--- qa_qc_lib/cubes/qa_qc_cubes.py
+++ qa_qc_lib/cubes/qa_qc_cubes.py
@@ -1,7 +1,7 @@
 from qa_qc_lib.data_reader import QA_QC_grdecl_parser
+from qa_qc_lib.qa_qc_main import QA_QC_main, Type_Status
 import numpy as np
 import datetime
-import logging
 
 SupportTypePetrelDict = {
     "Permeability_NP4": "PERMX",
@@ -19,39 +19,12 @@ SupportTypePetrelDict = {
     "GOC_NP4": "ABOVECONTACT",
 }
 
-"""
-class Report_Creator():
 
-    def __init__(self, file_report_path: str, req_reports: list[bool,bool,bool]):
+class QA_QC_cubes(QA_QC_main):
 
-    def
-"""
-
-class CustomAdapter(logging.LoggerAdapter):
-    def process(self, msg, kwargs):
-        context = kwargs.pop('file', self.extra['file'])
-        return '%s | [%s]' % (msg, context), kwargs
-
-class QA_QC_cubes(object):
-
-    def __init__(self, grid_path: str, logging_setting_path="../../report", logging_setting_file_name= None):
+    def __init__(self, grid_path: str, logging_setting_path="../../report", logging_setting_file_name=None):
+        QA_QC_main.__init__(self)
         self.grid_file = grid_path
-        if logging_setting_file_name is None:
-            self.logger = self.__setup_logging(logging_setting_path, self.__class__.__name__)
-        else:
-            self.logger = self.__setup_logging(logging_setting_path, logging_setting_file_name)
-
-        self.logger.info("Start module QA_QC_cubes")
-
-    def __setup_logging(self, path: str, file_name: str) -> logging:
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
-        logging.basicConfig(
-            filename=f"{path}/{timestamp}-{file_name}.log",
-            format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(message)s",
-            datefmt='%H:%M:%S',
-        )
-
-        return CustomAdapter(logging.getLogger(self.__class__.__name__), {'file': None})
 
     def __pars_wrong_values(self, wrong_list: list[
         list[int or float],
@@ -62,14 +35,14 @@ class QA_QC_cubes(object):
             result += f"value:{wrong_list[0][i]} " + f"index: {wrong_list[1][i]}\n"
         return result
 
-    def __test_porosity(self, array: np.array) -> tuple[
+    def __test_range_data(self, array: np.array, lambda_list: list[any]) -> tuple[
         bool,
         list[
             list[int or float],
             list[int]
         ] or None]:
 
-        mask_array = (array < 0) + (array > 0.476)
+        mask_array = (sum(func(array) for func in lambda_list)).astype(dtype=bool)
 
         if not any(mask_array):
             return True, None
@@ -83,14 +56,73 @@ class QA_QC_cubes(object):
     def test_open_porosity(self, file_path: str):
         data, key_petrel, err = QA_QC_grdecl_parser(self.grid_file, file_path=file_path).Get_Model()
         if err is not None:
-            self.logger.error(err.message)
+            self.update_report(self.generate_report_text(err.message, Type_Status.NotRunning))
             return
 
-        flag, wrong_data = self.__test_porosity(data.GRDECL_Data.SpatialDatas[SupportTypePetrelDict[key_petrel]])
+        flag, wrong_data = self.__test_range_data(
+            data.GRDECL_Data.SpatialDatas[SupportTypePetrelDict[key_petrel]],
+            [lambda x: x < 0, lambda x: x > 0.476])
+
         if flag:
-            self.logger.info("OK")
+            self.update_report(self.generate_report_text("", Type_Status.Passed.value))
         else:
-            self.logger.error(f"Данные \n {self.__pars_wrong_values(wrong_data)}  лежат не в интервале от 0 до 47,6")
+            self.update_report(self.generate_report_text(f"Данные \n {self.__pars_wrong_values(wrong_data)}  лежат не в интервале от 0 до 47,6", Type_Status.NotPassed))
+
+        self.generate_test_report(file_name=self.__class__.__name__, file_path="../../report", data_name=file_path)
+
+    def test_permeability(self, file_path: str):
+        data, key_petrel, err = QA_QC_grdecl_parser(self.grid_file, file_path=file_path).Get_Model()
+        if err is not None:
+            self.update_report(self.generate_report_text(err.message, Type_Status.NotRunning))
+            return
+
+        flag, wrong_data = self.__test_range_data(
+            data.GRDECL_Data.SpatialDatas[SupportTypePetrelDict[key_petrel]],
+            [lambda x: x < 0])
+
+        if flag:
+            self.update_report(self.generate_report_text("", Type_Status.Passed.value))
+        else:
+            self.update_report(self.generate_report_text(
+                f"Данные \n {self.__pars_wrong_values(wrong_data)}  < 0",
+                Type_Status.NotPassed))
+
+    def test_range_data(self, file_path: str):
+        data, key_petrel, err = QA_QC_grdecl_parser(self.grid_file, file_path=file_path).Get_Model()
+        if err is not None:
+            self.update_report(self.generate_report_text(err.message, Type_Status.NotRunning))
+            return
+
+        flag, wrong_data = self.__test_range_data(
+            data.GRDECL_Data.SpatialDatas[SupportTypePetrelDict[key_petrel]],
+            [lambda x: x < 0, lambda x: x > 1])
+
+        if flag:
+            self.update_report(self.generate_report_text("", Type_Status.Passed.value))
+        else:
+            self.update_report(self.generate_report_text(
+                f"Данные \n {self.__pars_wrong_values(wrong_data)}  лежат не в интервале от 0 до 1 ",
+                Type_Status.NotPassed))
+
+        self.generate_test_report(file_name=self.__class__.__name__, file_path="../../report", data_name=file_path)
 
 
-test = QA_QC_cubes("../../data/grdecl_data/GRID.GRDECL").test_open_porosity("../../data/grdecl_data/input/Poro.GRDECL.grdecl")
+    def test_integer_data(self, file_path: str):
+        data, key_petrel, err = QA_QC_grdecl_parser(self.grid_file, file_path=file_path).Get_Model()
+        if err is not None:
+            self.update_report(self.generate_report_text(err.message, Type_Status.NotRunning))
+            return
+
+        flag, wrong_data = self.__test_range_data(
+            data.GRDECL_Data.SpatialDatas[SupportTypePetrelDict[key_petrel]],
+            [lambda x: x == 0, lambda x: x == 1])
+
+        if flag:
+            self.update_report(self.generate_report_text("", Type_Status.Passed.value))
+        else:
+            self.update_report(self.generate_report_text(
+                f"Данные \n {self.__pars_wrong_values(wrong_data)}  не целочисленные",
+                Type_Status.NotPassed))
+
+        self.generate_test_report(file_name=self.__class__.__name__, file_path="../../report", data_name=file_path)
+QA_QC_cubes("../../data/grdecl_data/GRID.GRDECL").test_open_porosity("../../data/grdecl_data/input/Poro.GRDECL.grdecl")
