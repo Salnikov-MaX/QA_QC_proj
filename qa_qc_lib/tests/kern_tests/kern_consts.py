fcbceee5a66f9c6d38a3e627e6deacf8293fd162
Abramov Nikita <nabramov19@gmail.com>
2023-11-10 14:34:17 +0700

    Добавлен test_general_dependency_checking Добавлен класс для сохранения констант

---
+++ qa_qc_lib/tests/kern_tests/kern_consts.py
@@ -0,0 +1,10 @@
+class KernConsts:
+    def __init__(self) -> None:
+        self.general_dependency_checking_wrong ="Высокая дисперсия данных"
+        self.general_dependency_checking_accepted ="Низкая дисперсия данных"
+        self.check_data_empty_wrong = "Передан пустой массив"
+        self.check_data_not_array_wrong = "Передан не массив"
+        self.check_data_not_int_wrong = "Содержит не числовое значение"
+        self.check_data_has_nan_wrong = "Содержит nan"
+        self.monotony_wrong = "Нарушена монотонность глубины"
+        self.monotony_accepted = "Глубина монотонно возрастает"
