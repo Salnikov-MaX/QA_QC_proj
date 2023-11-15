a54a2b994f91814776b8a653ca86de4bb1b922b5
Rucard <www.carat@list.ru>
2023-11-15 15:28:47 +0700

    Добавлен cubes_tools.py

---
+++ qa_qc_lib/tools/cubes_tools.py
@@ -0,0 +1,116 @@
+#########################################################################################################
+#######################| РЕАЛИЗАЦИЯ ВСПОМОГАТЕЛЬНЫХ ФУНКЦИЙ QA_QC_cubes |##############################
+#########################################################################################################
+
+import numpy as np
+import inspect
+
+class CubesTools:
+    def __init__(self):
+        pass
+
+    def __remove_comment_lines(self, data: str, commenter: str = '--') -> str:
+        """
+        Функция для удаления строк комментариев из текстового документа
+
+        Args:
+            data (string): текстовый документ , переведенный в строку
+            commenter (string): ключ с которого начинается строка-комментарий
+
+        Returns:
+            str: Обработанная строка
+        """
+
+
+        data_lines = data.strip().split('\n')
+        newdata = []
+        for line in data_lines:
+            if not line.strip():
+                continue
+            elif line.find(commenter) != -1:
+                newline = line[0:line.find(commenter)].strip()
+                if len(newline) == 0:
+                    continue
+                newdata.append(newline)
+            else:
+                newdata.append(line)
+        return '\n'.join(newdata)
+
+    def find_key(self, file_path: str) -> [bool, str]:
+        """
+        Функция для поиска ключей в файлах GRDECL
+
+        Args:
+            file_path (str): путь к файлу
+
+        Returns:
+            bool: имеется ли ключ
+            str: значение ключа
+        """
+
+        with open(file_path, 'r') as f:
+            contents = f.read()
+            contents = self.__remove_comment_lines(contents, commenter='--')
+            contents_in_block = contents.strip().split('/')
+            contents_in_block = [x for x in contents_in_block if x]
+            NumKeywords = len(contents_in_block)
+            if NumKeywords > 2 or NumKeywords == 0:
+                return False, ""
+            else:
+                return True, contents_in_block[NumKeywords - 1].split()[0]
+
+    def generate_wrong_actnum(self,wrong_list: np.array, save_path: str = '.', name: str = "QA/QC"):
+        """
+        Функция для генерации GRDECL файла из массива (для генерации акнтуном которые моказывают ошибочные точки)
+
+        Args:
+            wrong_list (np.array): массив со значением точек
+            save_path (string): Путь куда созранить файл
+            name (string): Название файла
+        """
+
+        wrong_data = wrong_list.astype(dtype=int)
+        result_data = "-- Generated QA/QC\nACTNUM\n"
+        counter = 0
+        for index in range(len(wrong_data) - 2):
+            if wrong_data[index] == wrong_data[index + 1]:
+                counter += 1
+            else:
+                if counter != 0:
+                    result_data += f"{counter + 1}*{wrong_data[index]} "
+                    counter = 0
+                else:
+                    result_data += f"{wrong_data[index]} "
+
+        if wrong_data[len(wrong_data) - 1] == wrong_data[len(wrong_data) - 2]:
+            result_data += f"{counter + 2}*{wrong_data[len(wrong_data) - 1]} \\"
+        else:
+            s = ""
+            if counter == 0:
+                s += f"{wrong_data[len(wrong_data) - 2]}"
+            else:
+                s += f"{counter + 1}*{wrong_data[len(wrong_data) - 2]}"
+            result_data += f"{s} {wrong_data[len(wrong_data) - 1]} \\"
+
+        with open(f"{save_path}/{name}_WRONG_ACTNUM.GRDECL", 'w') as f:
+            f.write(result_data)
+            f.close()
+
+        print(f"Файл WRONG_ACTNUM сохранён по пути: {save_path}")
+
+    def get_cluster_dates(self, data1, data2, lit_data):
+        litatype_unique_data = np.unique(lit_data)
+        return {value: data1[np.where(lit_data == value)] for value in litatype_unique_data}, {
+            value: data2[np.where(lit_data == value)] for value in litatype_unique_data}
+
+    def conver_n3d_to_n1d(self, cube):
+        return np.ravel(cube, order='F').reshape((1, -1))[0]
+
+    def conver_n1d_to_n3d(self, grid, vector):
+        i_max = grid.ncol
+        j_max = grid.nrow
+        k_max = grid.nlay
+        return np.reshape(vector, (i_max, j_max, k_max), order='F')
+
+
+
