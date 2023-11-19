6c9abe11c1af4445f5dc8d83b8fbf9f1f84cf735
kos4v <kosach_1@live.com>
2023-11-20 00:55:49 +0700

    1

--- qa_qc_lib/graph/test_launch.py
+++ qa_qc_lib/graph/test_launch.py
@@ -5,6 +5,9 @@ import time
 from datetime import datetime
 
 from dataclasses import dataclass
+from json import JSONEncoder
+
+import numpy
 from dacite import from_dict
 from typing import List
 
@@ -16,7 +19,6 @@ from qa_qc_lib.tests.seismic_tests.seismic import QA_QC_seismic
 
 @dataclass
 class TestConfig:
-    test_name: str
     priority_data_for_launch: List[DataInfo]
     all_data_for_launch: List[List[DataInfo]]
     ready_for_launch: bool
@@ -24,80 +26,94 @@ class TestConfig:
 
 
 class LaunchTest:
-    def __init__(self, test_config: TestConfig, graph_test: GraphTest):
+    def __init__(self, test_config: TestConfig):
         self.test_config: TestConfig = test_config
-        self.graph_test: GraphTest = graph_test
 
     def start_test(self, result_dir: str):
-        switch = {
+        switch_class = {
             EnumQAQCClass.Kern: LaunchTestKern,
-            EnumQAQCClass.Seismic: QA_QC_seismic,
+            EnumQAQCClass.Seismic: LaunchSeismic,
         }
 
-        test_class = switch[self.graph_test.test_class_name]
-        test_obj: LaunchTest = test_class(self.test_config, self.graph_test)
+        test_class = switch_class[self.test_config.graph_test.test_class_name]
+        test_obj: LaunchTest = test_class(self.test_config)
 
         if not os.path.isdir(result_dir):
             raise Exception(f'Директории: {result_dir} не существует!')
 
-        test_method = getattr(test_obj, self.graph_test.test_method_name)
+        test_method = getattr(test_obj, self.test_config.graph_test.test_method_name)
 
         result_data: dict = test_method()
-        now_time = datetime.now().strftime("%Y_%m_%d_%H:%M:%S")
-        result_path = os.path.join(result_dir, f'{self.test_config.test_name}_{now_time}.json', 'w+')
+        result_data = result_data if result_data is not None else {
+            "test_method": self.test_config.graph_test.test_method_name}
 
-        with open(result_path, 'w+') as file:
-            json.dump(result_data, file)
+        now_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        test_name = self.test_config.graph_test.test_method_name
+        result_path = os.path.join(result_dir, f'{test_name}_{now_time}.json')
 
-        return result_data
+        with open(result_path, 'w+', encoding='utf-8') as file:
+            json.dump(result_data, file, ensure_ascii=False,
+                      default=lambda o: f"<<non-serializable: {type(o).__qualname__}>>")
+
+        return test_name, self.test_config
 
     @staticmethod
-    def read_configs(test_config_path: str) -> List[TestConfig]:
+    def read_configs(test_config_path: str, graph_list: List[GraphTest]) -> List[TestConfig]:
         with open(test_config_path, 'r', encoding='utf-8') as file:
             test_configs = json.load(file)
+
+        graph_dict = {node.test_code_name: node for node in graph_list}
 
         tests = []
         for data_config in test_configs:
             for test_config in data_config['tests']:
                 if test_config['ready_for_launch']:
-                    tests.append(from_dict(data_class=TestConfig, data=test_config))
+                    tests.append(from_dict(data_class=TestConfig,
+                                           data={**test_config,
+                                                 "graph_test": graph_dict[test_config['test_name']]}))
 
         return tests
 
 
 class LaunchTestKern(LaunchTest):
-    def test_open_porosity(self):
-        self.config.priority_data_for_launch
+    def test_open_porosity(self) -> dict:
+        data_info: [DataInfo] = self.test_config.priority_data_for_launch
         test_class = QA_QC_kern(
-            # porosity_open=
+            file_path='result',
+            porosity_open=data_info[0]
         )
-        test_class.test_porosity_open_vs_swl()
+        return test_class.test_porosity_open_vs_swl()
 
-    def check_input(self):
+    def check_input(self) -> dict:
         pass
 
-    def test_monotony(self):
+    def test_monotony(self) -> dict:
+        data_info: [DataInfo] = self.test_config.priority_data_for_launch
+        test_class = QA_QC_kern(
+            file_path='result',
+            depth=data_info[0]
+        )
+        return test_class.test_monotony()
+
+    def test_residual_water_saturation(self) -> dict:
         pass
 
-    def test_residual_water_saturation(self):
+    def test_data_tampering(self) -> dict:
         pass
 
-    def test_data_tampering(self):
+    def test_sg(self) -> dict:
         pass
 
-    def test_sg(self):
+    def test_sgl(self) -> dict:
         pass
 
-    def test_sgl(self):
+    def test_so(self) -> dict:
         pass
 
-    def test_so(self):
+    def test_sogcr(self) -> dict:
         pass
 
-    def test_sogcr(self):
-        pass
-
-    def test_parallel_permeability(self):
+    def test_parallel_permeability(self) -> dict:
         pass
 
 
--- scenarios_for_testing/graph_scenarios/testing_data/start.py
+++ scenarios_for_testing/graph_scenarios/testing_data/start.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 
 from qa_qc_lib.graph.graph import GraphTest
@@ -5,14 +6,19 @@ from qa_qc_lib.graph.test_launch import LaunchTest, TestConfig
 
 if __name__ == '__main__':
     # launch_test = LaunchTest()
-    test_config_path = 'data/result.json'
+    test_config_path = '../generate_test_configs_from_available_data/data/result.json'
     graph_path = '../../../qa_qc_lib/graph/config/graph.json'
-    test_configs: List[TestConfig] = LaunchTest.read_configs(test_config_path)
 
     graph_dict: dict[str, GraphTest] = GraphTest.read_tests_info_file_as_dict(graph_path)
-    launch_tests: List[LaunchTest] = [LaunchTest(config, graph_dict[config.test_name]) for config in test_configs]
+    test_configs: List[TestConfig] = LaunchTest.read_configs(test_config_path, list(graph_dict.values()))
+
+    launch_tests: List[LaunchTest] = [LaunchTest(config) for config in test_configs]
+
+    results_dir = 'results'
+    if not os.path.isdir(results_dir):
+        os.mkdir(results_dir)
 
     for test in launch_tests:
-        print(test)
+        print(test.start_test(results_dir))
 
     exit(0)
