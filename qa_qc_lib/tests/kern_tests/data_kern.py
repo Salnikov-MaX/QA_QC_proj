70d3ad5818791d3ba10ac89367772d49dba96f86
Abramov Nikita <nabramov19@gmail.com>
2023-12-07 22:44:28 +0700

    Исправлен метод mark_errors Исправлена покраска аномальных значений Исправлена иерархия тестов в kern Исправлен метод logarithm_function_visualization Исправлен метод logarithmic_function Поправлены пользовательские сценарии Перемещены файлы

--- qa_qc_lib/tests/kern_tests/data_kern.py
+++ qa_qc_lib/tests/kern_tests/data_kern.py
@@ -4,9 +4,10 @@ from qa_qc_lib.tests.kern_tests.kern_consts import KernConsts
 
 
 class DataKern:
-    def __init__(self, file_path):
+    def __init__(self, file_path=r"..\..\..\data\core_data\post_test_table.xlsx"):
         self.consts = KernConsts()
         self.file_path = file_path
+        self.dict_array = []
 
     def __color_cells(self, df, error_indices, column, color='red'):
         """
@@ -60,28 +61,35 @@ class DataKern:
         filtered_df = post_test_df[column_names]
         return filtered_df
 
-    def mark_errors(self, column_name, test_name, error_description, error_mask, md):
+    def mark_errors(self):
         """
-        Метод для окрашивания аномальных значений
+        Метод для окрашивания аномальных значений.
+
         Args:
-            column_name(array[string]): название колонки
-            test_name(string): название теста
-            error_description(string): описание ошибки
-            error_mask(np.ndarray[int]): маска с результатом
-            md(np.ndarray[float/int]): массив с глубиной
-
+            self.dict_array - структура массива [{"test_name":[param_name,
+                                                              error_desc,
+                                                              error_mask,
+                                                              md]}] - нужен для пометки значений
         Returns:
-
+            excel - таблица с покрашенными значениями и отмеченными ошибками
         """
-
         post_test_df = pd.read_excel(self.file_path)
-        error_indices = md[error_mask == 1]
-        post_test_df[test_name] = np.nan
-        post_test_df[test_name] = post_test_df[test_name].astype(object)
-        post_test_df.loc[post_test_df.index.isin(
-            error_indices), test_name] = error_description
 
-        styled_df = post_test_df.style.apply(self.__color_cells, axis=None, error_indices=error_indices,
-                                             column=column_name)
-        styled_df.to_excel(self.file_path,
-                           sheet_name='Sheet1', index=False)
+        for dict_array in self.dict_array:
+            for test_name, values in dict_array.items():
+                column_name, error_description, error_mask, md = values
+                error_indices = md[error_mask == 1]
+                if not test_name in post_test_df.columns:
+                    post_test_df[test_name] = np.nan
+                post_test_df[test_name] = post_test_df[test_name].astype(object)
+                post_test_df.loc[post_test_df.index.isin(error_indices), test_name] = error_description
+
+        styled_df = post_test_df.style
+        for dict_array in self.dict_array:
+            for test_name, values in dict_array.items():
+                column_name, _, error_mask, md = values
+                error_indices = md[error_mask == 1]
+                styled_df = styled_df.apply(self.__color_cells, axis=None, error_indices=error_indices,
+                                            column=column_name)
+
+        styled_df.to_excel(self.file_path, sheet_name='Sheet1', index=False)
